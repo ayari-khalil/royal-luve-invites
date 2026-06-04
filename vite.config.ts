74ac63ec9540a030016977ee82e580769612c85a
khalil ayari <ayari2014khalil@gmail.com>
2026-06-04 13:40:02 +0100

    fix tailwind styles for static frontend

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,7 @@ export default defineConfig({
     }),
     react(),
     tsconfigPaths(),
+    tailwindcss(),
   ],
   server: {
     port: 8080,
